d516a37dc7c646dd4b1f181b0a855386727671d6
Brad Breiten <jonathan@breiten.us>
2022-02-02 15:11:41 -0700

    Add route to results page

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root 'landing#index'
   get '/register', to: 'landing#register'
   get '/users/:user_id/discover', to: 'movies#discover'
+  get '/users/:user_id/results', to: 'movies#results'
 
   resources :users, only: [:new, :create, :show] do
 
