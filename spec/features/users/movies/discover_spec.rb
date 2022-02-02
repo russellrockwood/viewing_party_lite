2163008b8c5c1195253f0089e1d1ea9f65d13842
Brad Breiten <jonathan@breiten.us>
2022-02-02 16:15:28 -0700

    Add stubs for API calls in tests

--- spec/features/users/movies/discover_spec.rb
+++ spec/features/users/movies/discover_spec.rb
@@ -27,6 +27,10 @@ RSpec.describe 'Discover Movies Page', type: :feature do
 
   context 'When I visit discover movies page' do
     scenario 'I click on top movies link and am taken to results page' do
+      top_rated_response = File.read('./spec/support/movie_api_responses/top_rated_response.json')
+
+      stub_request(:get, "https://api.themoviedb.org/3/movie/top_rated?api_key=#{ENV['movie_api_key']}&language=en-US&page=1").to_return(body: top_rated_response, status: 200)
+
       click_button 'Discover Top Rated Movies'
       expect(page.status_code).to eq 200
       expect(current_path).to include("/users/#{user_id}/results")
@@ -34,6 +38,12 @@ RSpec.describe 'Discover Movies Page', type: :feature do
     end
 
     scenario 'I fill in the search form and click search and am taken to the results page' do
+      search_fox_response = File.read('./spec/support/movie_api_responses/search_fox_response.json')
+
+      query = 'Fox'
+
+      stub_request(:get, "https://api.themoviedb.org/3/search/movie?api_key=#{ENV['movie_api_key']}&language=en-US&query=#{query}&page=1&include_adult=false").to_return(body: search_fox_response, status: 200)
+
       fill_in :search, with: 'Fox'
       click_button 'Search'
 
